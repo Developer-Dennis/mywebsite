6e607d8ba0b5072a46049e53b574d3c5718c9ecd
Developer-Dennis <denniskipkogei11@gmail.com>
2022-10-07 16:58:13 +0300

    created a footer for the webpage

---
+++ src/components/footer/footer.js
@@ -0,0 +1,11 @@
+import './footer.css'
+
+const Footer = () => (
+    <>
+    <footer>
+      <p>Copyright © 2022 <a href="#">Dennis Kemboi</a> All rights reserved</p>
+      </footer>
+      </>
+  );
+  
+  export default Footer;
