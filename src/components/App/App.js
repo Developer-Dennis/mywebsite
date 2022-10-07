88c23282aed7fb0290eda4bb53595db05f728ec8
Developer-Dennis <denniskipkogei11@gmail.com>
2022-10-07 16:48:11 +0300

    added routes to route directory

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,26 @@
+
 import navstyles from '../navbar/navbar.css';
 import Navbar from '../navbar/navbar.js';
 import Home from '../home/home.js';
 import Homestyles from '../home/home.css';
 import Services from '../services/services';
 import About from '../aboutme/aboutme.js';
-// import {Button, Input,Form} from './contact/contact.js'
+import Contact from '../contact/contact.js'
+import Footer from '../footer/footer';
 
 function App() {
   return(
     <>
+
+
+
+
   <Navbar />
   <Home />
   <Services />
   <About />
-   {/* <Button /> */}
+  <Contact />
+  <Footer />
   </>
   )
   
