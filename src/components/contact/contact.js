f244e5b866bcfffee318c9897cbafa8920a7f624
Developer-Dennis <denniskipkogei11@gmail.com>
2022-10-11 14:35:14 +0300

    created contact page so that clients can easily contact me

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -7,9 +7,9 @@ const Contact = () => {
         <>
         <h2>Contact Us</h2>
         <form>      
-            <input name="name" type="text" class="feedback-input" placeholder="FullNames" />   
-            <input name="email" type="text" class="feedback-input" placeholder="Email" />
-            <textarea name="text" class="feedback-input" placeholder="Message"></textarea>
+            <input name="name" type="text" className="feedback-input" placeholder="FullNames" />   
+            <input name="email" type="text" className="feedback-input" placeholder="Email" />
+            <textarea name="text" className="feedback-input" placeholder="What project do you want to develop we can help"></textarea>
             <input type="submit" value="SUBMIT"/>
         </form>
    </>
