343cbc5710fcb53555fd9cc635aeb80b94bdc4c2
Developer-Dennis <denniskipkogei11@gmail.com>
2022-10-11 14:31:58 +0300

    added navigation bar to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,32 @@
-
 import navstyles from '../navbar/navbar.css';
-import Navbar from '../navbar/navbar.js';
 import Home from '../home/home.js';
 import Homestyles from '../home/home.css';
-import Services from '../services/services';
+import Services from '../services/services.js';
 import About from '../aboutme/aboutme.js';
 import Contact from '../contact/contact.js'
 import Footer from '../footer/footer';
+import Navbar from '../navbar/navbar.js';
+import React from 'react';
 
 function App() {
   return(
     <>
-
-
-
-
-  <Navbar />
+    <div className='list'>
+    
+    <ul id="nav-list">
+      <li><a href="http://localhost:3000/"> Home</a></li>
+      <li><a href="http://localhost:3000/services">Services </a></li>
+      <li><a href="http://localhost:3000/aboutme"> About Me</a></li>
+      <li><a href="http://localhost:3000/contact"> Contact</a></li>
+    </ul>
+    <Navbar />
+    </div>
   <Home />
   <Services />
   <About />
   <Contact />
   <Footer />
+  
   </>
   )
   
