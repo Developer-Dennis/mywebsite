c7419ba914fc11081e57a908a49a6437d63099ec
Developer-Dennis <denniskipkogei11@gmail.com>
2022-10-15 15:24:20 +0300

    imported the navbar and called it

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router,Switch, Route, Routes } from 'react-router-dom'; 
 import '../App/App.css'
-import navstyles from '../navbar/navbar.css';
 import Home from '../home/home.js';
-import Homestyles from '../home/home.css';
 import Services from '../services/services.js';
 import About from '../aboutme/aboutme.js';
 import Contact from '../contact/contact.js'
 import Footer from '../footer/footer';
 import Navbar from '../navbar/navbar.js';
-import React from 'react';
+
 
 function App() {
   return(
-    <>
-      <nav>
-      <ul className="list">
-        <li ><a  className="items" href="http://localhost:3000/"> Home</a></li>
-        <li ><a className="items" href="http://localhost:3000/services">Services </a></li>
-        <li ><a className="items" href="http://localhost:3000/aboutme"> About Me</a></li>
-        <li ><a className="items" href="http://localhost:3000/contact"> Contact</a></li>
-      </ul>
-      <Navbar />
-      </nav>
+
+<>
+    <Router >
+    <Navbar />
+    <Routes>
+    <Route className="items" path='/home' element={<Home />}/>
+    <Route  className="items" path='/services' element={<Services />}/>
+    <Route className="items" path='/aboutme' element={<About />}/>
+    <Route className="items" path='/contact' element={<Contact />}/>
+    </Routes>
+    </Router>
+    
+
+    
+     
   <Home />
   <Services />
   <About />
