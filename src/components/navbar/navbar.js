0ced5cc02bec3910e6865acd10029e818c27e60d
Developer-Dennis <denniskipkogei11@gmail.com>
2022-10-18 12:59:52 +0300

    created my navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,37 +5,46 @@ import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
 
 
-
 function Navbar(){
   const [open, setOpen] = useState(false);
   return(
-  <nav className='navbar'>
-    <Link to="/" className='nav-logo' onClick={() => setOpen(false)}>
-      Dennis Kemboi
-    </Link>
-    <ul className={open ? 'nav-link active' : 'nav-links'}>
-    <li className="nav-item">
-          <Link to="/" className="nav-link" onClick={() => setOpen(false)}>
-            Home
-          </Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/services" className="nav-link" onClick= {() => setOpen(false)}>
-          Services
-          </Link>
-      </li>
-      <li className="nav-item">
-          <Link to="/aboutme" className="nav-link" onClick={() => setOpen(false)}>
-            About Me
-          </Link>
-      </li>
-      <li className="nav-item">
-          <Link to="/contact" className="nav-link" onClick={() => setOpen(false)}>
-            Contact
-          </Link>
-      </li>
-    </ul>
-  </nav>
+  <>
+    <script src="script.js" defer></script>
+    <nav className='navbar'>
+      <Link to="/" className='nav-logo' onClick={() => setOpen(false)}>
+        Dennis Kemboi
+      </Link>
+      <a href="#" className='toggle-button'>
+        <span className='bar'></span>
+        <span className='bar'></span>
+        <span className='bar'></span>
+      </a>
+      <div className='navbar-links'>
+        <ul className={open ? 'nav-link active'  :'nav-links'}>
+        <li className="nav-item">
+              <Link to="/" className="nav-link" onClick={() => setOpen(false)}>
+                Home
+              </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/services" className="nav-link" onClick= {() => setOpen(false)}>
+              Services
+              </Link>
+          </li>
+          <li className="nav-item">
+              <Link to="/aboutme" className="nav-link" onClick={() => setOpen(false)}>
+                About 
+              </Link>
+          </li>
+          <li className="nav-item">
+              <Link to="/contact" className="nav-link" onClick={() => setOpen(false)}>
+                Contact
+              </Link>
+          </li>
+        </ul>
+        </div>
+    </nav>
+  </>
   )
 }
 export default Navbar;
